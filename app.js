4fd3a041e65a495837bc15117e31fdc3033d2979
dance-elevate <geibkendra@gmail.com>
2026-07-29 16:57:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
-// ELEVATE Welcome Message
+
+// 🎀 ELEVATE App JavaScript
+
+
+// Welcome message
 window.onload = function() {
-    alert("Welcome to ELEVATE 🎀\n\nThis is Version 1!\nMore features coming soon ✨");
+    alert(
+        "Welcome to ELEVATE 🎀\n\n" +
+        "This is Version 1!\n" +
+        "More features are coming soon ✨\n\n" +
+        "Keep training. Keep growing."
+    );
 };
 
 
-// Training Buttons
+// Training button function
 function startTraining(trainingName) {
-    alert("Starting: " + trainingName + " 🎀");
 
+    alert(
+        "Starting: " + trainingName + " 🎀\n\n" +
+        "Your training session has been selected!"
+    );
+
+    // Save current training
     localStorage.setItem("currentTraining", trainingName);
+
+}
+
+
+// Get saved training (for future pages)
+function getCurrentTraining() {
+
+    let training = localStorage.getItem("currentTraining");
+
+    return training;
+
+}
+
+
+// Clear training (future use)
+function clearTraining() {
+
+    localStorage.removeItem("currentTraining");
+
 }
