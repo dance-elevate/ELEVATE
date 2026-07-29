3a352416274e766ce825052e75629eebc0c4937c
dance-elevate <geibkendra@gmail.com>
2026-07-29 16:35:46 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const startBtn = document.getElementById("startBtn");
 
+if (startBtn) {
+  startBtn.addEventListener("click", () => {
+    startBtn.textContent = "Let's Dance! 🩰";
+    startBtn.style.backgroundColor = "#d98aa5";
+
+    setTimeout(() => {
+      alert("🎀 Welcome to ELEVATE!\n\nThis is Version 1.0 of your dance training website. More features are coming soon!");
+    }, 300);
+  });
+}
